27b4d0374ed2864084fd2e671a41afde0869fdf8
ayushroyl <ayushroy0753@gmail.com>
2024-10-08 21:25:55 +0530

    update tailwind CSS config

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import './App.css';
+import './index.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Nav from './components/Nav.jsx';
+import Home from './pages/Home.js';
+import About from './components/About.jsx';
+import Team from './components/Team.jsx';
+import Footer from './components/Footer.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <p>hello world</p>
-    </div>
+    <>
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                {" "}
+                <Home /> <About /> <Team />{" "}
+              </>
+            }
+          />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
   );
 }
 
