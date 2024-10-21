3397b4997198bb63bf0cadb7e57e6869862a67b8
ayushroyl <ayushroy0753@gmail.com>
2024-10-21 11:04:12 +0530

    editsadmins

--- src/pages/superadmin.jsx
+++ src/pages/superadmin.jsx
@@ -183,7 +183,7 @@ const SuperAdminDashboard = () => {
                     Welcome {superadminName}
                 </h2>
                 <div>
-                <a className='font-medium bg-green-800 items-center text-white' href='https://admin-panel-tan-three.vercel.app/login'>Applied Perfomer</a>
+                <a className='font-medium bg-green-800 border-2 rounded-sm text-white' href='https://admin-panel-tan-three.vercel.app/login'><span color='red'>New</span> Applied Perfomer</a>
                 </div>
                 {/* Display the message */}
                 {showMessage && (
