7e354ef1cb2b32ef49e1939656d9ff70578900e4
ayushroyl <ayushroy0753@gmail.com>
2024-10-21 08:58:39 +0530

    admin

--- src/pages/admin.jsx
+++ src/pages/admin.jsx
@@ -68,6 +68,7 @@ const AdminDashboard = () => {
     const usersRef = ref(database, 'users');
     const newUserRef = push(usersRef);
     set(newUserRef, { ...userForm, id: newUserRef.key, approved: false });
+    setUsers((prevUsers) => [newUser, ...prevUsers]);
     showPopup(`User ${userForm.name} added successfully.`);
     setUserForm({ name: '', class: '', roll: '', mobile: '', seatNo: '', paymentMode: 'cash' });
   };
