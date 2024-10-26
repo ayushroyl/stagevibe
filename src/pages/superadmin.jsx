5fb01904ec8f25746736ec513978bff83310812e
Ayush Roy <143392455+ayushroyl@users.noreply.github.com>
2024-10-26 13:40:16 +0530

    Update superadmin.jsx

--- src/pages/superadmin.jsx
+++ src/pages/superadmin.jsx
@@ -376,12 +376,12 @@ const SuperAdminDashboard = () => {
                                         >
                                             Invite
                                         </a>
-                                        <button
+{/*                                         <button
                                             onClick={() => handleDeleteUser(user.id)}
                                             className="bg-red-500 text-white px-4 py-1 rounded-md hover:bg-red-600 transition"
                                         >
                                             Delete
-                                        </button>
+                                        </button> */}
                                     </div>
                                 </li>
                             ))}
